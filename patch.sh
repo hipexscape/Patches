1c94c9ea41b2df84602932601477de539abd182b
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2024-01-22 19:49:09 +0530

    patcher: Include `Fake Unname to v14.9 Patch`

--- patch.sh
+++ patch.sh
@@ -6,4 +6,7 @@ git -C "frameworks/av" am <<<"$(curl -sL "https://github.com/ArrowOS/android_fra
 # stagefright: remove HW_TEXTRUE usage from SurfaceMediaSource
 git -C "frameworks/av" am <<<"$(curl -sL "https://github.com/ArrowOS/android_frameworks_av/commit/a727c1f68fd30c8e6a4068db9dc26670d4a78f6c.patch")"
 
+# Kernel: Fake unname kernel to v4.19
+git -C "kernel/xiaomi/mt6768" am <<<"$(curl -sL "https://github.com/realme-mt6785-devs/android_kernel_realme_mt6785/commit/25801744bc0e67380287ffce56c1bb0671be3304.patch")"
+
 echo 'Patching done'
