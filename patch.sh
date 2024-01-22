ca0366d552cf01aa2ebb5eb406db013318000cce
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2024-01-22 19:43:51 +0530

    patcher: Drop `REThreaded`

--- patch.sh
+++ patch.sh
@@ -6,7 +6,4 @@ git -C "frameworks/av" am <<<"$(curl -sL "https://github.com/ArrowOS/android_fra
 # stagefright: remove HW_TEXTRUE usage from SurfaceMediaSource
 git -C "frameworks/av" am <<<"$(curl -sL "https://github.com/ArrowOS/android_frameworks_av/commit/a727c1f68fd30c8e6a4068db9dc26670d4a78f6c.patch")"
 
-# REThreaded: Use gen- and delete- textures on all render engines
-git -C "frameworks/native" am <<<"$(curl -sL "https://github.com/ArrowOS/android_frameworks_native/commit/1e483eea5cf3b4972939a313652aebac42a1561c.patch")"
-
 echo 'Patching done'
